3cbec6029df646e26489fe0bf43556b297046fee
cnguyen0691 <chaunguyen0691@gmail.com>
2019-05-10 09:58:43 -0400

    initial

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping ("/home")
+        public String home(){
+            return "home";
+        }
+    @RequestMapping ("/github")
+    public String github(){
+        return "github";
+    }
+    @RequestMapping ("/OOP")
+    public String OOP(){
+        return "OOP";
+    }
+    @RequestMapping ("/Authors")
+    public String Authors(){
+        return "Authors";
+    }
+}
+
